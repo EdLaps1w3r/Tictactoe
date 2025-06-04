84d7735fe173992fa7d490d978f156679a7629fc
EdLaps1w3r <edlaps1w3r@gmail.com>
2025-06-04 13:41:41 +0800

    Reworked js and html setup to allow for win check and more streamlined code.

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,77 +1,129 @@
-const Gameboard = (function () {
-    const gameboard = ["", "", "", "", "", "", "", "", ""];
-    const gameboardEmpty = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    const placeX = (position) => gameboard.splice(position, 1, "X");
-    const placeO = (position) => gameboard.splice(position, 1, "O");
-    return {gameboard, gameboardEmpty, placeX, placeO};
-})();
-
-const choice = (function () {
-    const player = 9;
-    const computer = (empty) => empty[Math.floor(Math.random() * empty.length)];
-    return {computer, player};
-})();
-
-
-const check = (function (){
-    const player = (choice) => Gameboard.gameboardEmpty.find((ele) => ele == choice);
-    const index = (choice) => Gameboard.gameboardEmpty.findIndex((ele) => ele == choice);
-    return {player, index};
-})();
-
-
-
-function playerChoose(num) {
-    thisSpace = document.getElementById(num);
-    choice.player = num;
-    choiceIsValid = check.player(choice.player);
-    if (isNaN(choiceIsValid) == false) {
-        Gameboard.placeX(choice.player);
-        thisSpace.innerHTML = 'X';
-        Gameboard.gameboardEmpty.splice(check.index(num), 1);
-        if (Gameboard.gameboardEmpty.length > 0) {
-            let computerChoice = choice.computer(Gameboard.gameboardEmpty);
-            console.log(computerChoice);
-            Gameboard.gameboardEmpty.splice(check.index(computerChoice), 1);
-            Gameboard.placeO(computerChoice);
-            document.getElementById(computerChoice).innerHTML = "O";
-        } else {
-            Alert("Game ended in a draw!");
-        };
+// Gameboard Module
+const Gameboard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];
+  
+    const getBoard = () => board;
+  
+    const resetBoard = () => {
+      for (let i = 0; i < board.length; i++) {
+        board[i] = "";
+      }
     };
-    console.log(Gameboard.gameboard);
-    console.log(Gameboard.gameboardEmpty);
-}
-
-
-
-function playTicTacToe() {
-    while (Gameboard.gameboardEmpty.length > 0){
-        let playerChoice = "i";
-        while (isNaN(playerChoice) == true) {
-            playerChoice = round.player(choice.player(Gameboard.gameboardEmpty))
-        };
-        Gameboard.placeX(playerChoice);
-        spaceIndex = Gameboard.gameboardEmpty.findIndex((ele) => ele == playerChoice);
-        Gameboard.gameboardEmpty.splice(spaceIndex, 1);
-        console.log(Gameboard.gameboard);
-        console.log(Gameboard.gameboardEmpty);
-        let computerChoice = "i";
-        if (Gameboard.gameboardEmpty.length != 0){
-            while (isNaN(computerChoice) == true) {
-                computerChoice = round.computer(choice.computer(Gameboard.gameboardEmpty));
-            };
-            Gameboard.placeO(computerChoice);
-            Gameboard.gameboardEmpty.splice(computerChoice, 1);
-            console.log(Gameboard.gameboard);
-            console.log(Gameboard.gameboardEmpty);
-        } else {
-            break;
+  
+    const placeMark = (index, mark) => {
+      if (board[index] === "") {
+        board[index] = mark;
+        return true;
+      }
+      return false;
+    };
+  
+    const isFull = () => board.every(cell => cell !== "");
+  
+    return { getBoard, resetBoard, placeMark, isFull };
+  })();
+  
+  // Player Factory
+  const Player = (name, mark) => {
+    return { name, mark };
+  };
+  
+  // Game Controller Module
+  const GameController = (() => {
+    const player1 = Player("Player 1", "X");
+    const player2 = Player("Player 2", "O");
+    let currentPlayer = player1;
+    let gameOver = false;
+  
+    const switchPlayer = () => {
+      currentPlayer = (currentPlayer === player1) ? player2 : player1;
+    };
+  
+    const checkWinner = () => {
+      const board = Gameboard.getBoard();
+      const winConditions = [
+        [0,1,2], [3,4,5], [6,7,8], // rows
+        [0,3,6], [1,4,7], [2,5,8], // columns
+        [0,4,8], [2,4,6]           // diagonals
+      ];
+  
+      for (const condition of winConditions) {
+        const [a, b, c] = condition;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+          gameOver = true;
+          return currentPlayer.name;
         }
-
+      }
+      if (Gameboard.isFull()) {
+        gameOver = true;
+        return "Tie";
+      }
+      return null;
     };
-    return "Done";
-};
-
-
-
+  
+    const playRound = (index) => {
+      if (gameOver) return;
+      if (Gameboard.placeMark(index, currentPlayer.mark)) {
+        const result = checkWinner();
+        if (result) {
+          DisplayController.showResult(result);
+        } else {
+          switchPlayer();
+        }
+      }
+    };
+  
+    const restartGame = () => {
+      Gameboard.resetBoard();
+      currentPlayer = player1;
+      gameOver = false;
+      DisplayController.render();
+      DisplayController.showResult("");
+    };
+  
+    return { playRound, restartGame };
+  })();
+  
+  // Display Controller Module
+  const DisplayController = (() => {
+    const boardElement = document.getElementById("gameboard");
+    const gameInfo = document.getElementById("game-info");
+    const restartBtn = document.getElementById("restart-btn");
+  
+    const render = () => {
+      const board = Gameboard.getBoard();
+      boardElement.innerHTML = "";
+      board.forEach((cell, index) => {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        if (cell !== "") {
+          square.textContent = cell;
+          square.classList.add("taken");
+        }
+        square.addEventListener("click", () => {
+          GameController.playRound(index);
+          render();
+        });
+        boardElement.appendChild(square);
+      });
+    };
+  
+    const showResult = (result) => {
+      if (result === "Tie") {
+        gameInfo.textContent = "It's a tie!";
+      } else if (result) {
+        gameInfo.textContent = `${result} wins!`;
+      } else {
+        gameInfo.textContent = "";
+      }
+    };
+  
+    restartBtn.addEventListener("click", () => {
+      GameController.restartGame();
+    });
+  
+    render(); // initial render
+  
+    return { render, showResult };
+  })();
+  
